cbb7aa2eccf7a366e79b4879fb7fe5ca615dc83b
signorpotti <varga.peter.mail@gmail.com>
2023-12-03 08:59:17 +0100

    D2

---
+++ D2/src/main/java/com/aoc/Main.java
@@ -0,0 +1,11 @@
+package com.aoc;
+
+import com.aoc.game.GameEvaluator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(GameEvaluator.evaluate("D2/src/main/resources/aoc2input.txt"));
+        System.out.println(GameEvaluator.calculatePower("D2/src/main/resources/aoc2input.txt"));
+    }
+}
